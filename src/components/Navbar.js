1f7d015fef5f9e396d78db26001cadb56a86a932
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2023-09-02 22:13:22 +0400

    just created

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+    return (  
+        <nav className="justify-self-end self-stretch row-span-2 bg-light-obsidian p-8 w-100 flex flex-col gap-50 justify-center items-center rounded-3xl">
+            <button className="cursor-pointer active:scale-95 hover:font-bold">Weather</button>
+            <button className="cursor-pointer active:scale-95 hover:font-bold">Cities</button>
+            <button className="cursor-pointer active:scale-95 hover:font-bold">Map</button>
+            <button className="cursor-pointer active:scale-95 hover:font-bold">Settings</button>
+        </nav>
+    );
+}
+ 
+export default Navbar;
